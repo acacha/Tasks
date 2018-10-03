b8ec87566795b360fb7df3122a086b3e7f908c88
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2018-10-03 18:01:00 +0200

    Sync

---
+++ vue-tasks/src/components/Tasks.vue
@@ -0,0 +1,17 @@
+<template>
+    <ul>
+        <li>Tasca 1</li>
+        <li>Tasca 2</li>
+        <li>Tasca 3</li>
+        <li>Tasca 4</li>
+        <li>Tasca 5</li>
+    </ul>
+</template>
+
+<script>
+
+</script>
+
+<styles>
+
+</styles>

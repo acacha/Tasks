f04ffda02f59f5f2f0cbd8be12b91e990eb5786f
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2018-10-22 14:17:19 +0200

    Sync

--- resources/js/app.js
+++ resources/js/app.js
@@ -13,7 +13,7 @@ const Vuetify = require('vuetify')
 window.Vue.use(Vuetify)
 
 require('vuetify/dist/vuetify.min.css')
-require('material-design-icons-iconfont/dist/material-design-icons.css')
+require('material-design-icons-iconfont/dist/material-design-icons.css') // Ensure you are using css-loader
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -24,7 +24,6 @@ require('material-design-icons-iconfont/dist/material-design-icons.css')
 window.Vue.component('example-component', require('./components/ExampleComponent.vue'))
 window.Vue.component('tasks', require('./components/Tasks.vue'))
 
+const AppComponent = require('./App.vue')
 // eslint-disable-next-line no-unused-vars
-const app = new window.Vue({
-  el: '#app'
-})
+const app = new window.Vue(AppComponent)

c59e537d35d830464fb1e7b0a823e6b68d197d22
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2018-12-18 21:17:27 +0100

    Sync

--- resources/js/components/TasksTags.vue
+++ resources/js/components/TasksTags.vue
@@ -1,13 +1,30 @@
 <template>
     <span>
-        <v-chip v-for="tag in task.tags" :key="tag.id" v-text="tag.name" :color="tag.color"></v-chip>
+        <v-chip v-for="tag in task.tags" :key="tag.id" v-text="tag.name" :color="tag.color" @dblclick="removeTag"></v-chip>
         <v-btn icon @click="dialog = true"><v-icon>add</v-icon></v-btn>
+        <v-btn icon @click="dialog = true"><v-icon>remove</v-icon></v-btn>
         <v-dialog v-model="dialog" width="500">
             <v-card>
                 <v-card-title>Afegiu etiquetes a la tasca</v-card-title>
                 <v-card-text>
-                    <v-combobox v-model="selectedTags" :items="tags">
-
+                    <v-combobox
+                            v-model="selectedTags"
+                            :items="tags"
+                            multiple
+                            chips
+                            item-text="name"
+                    >
+                        <template slot="selection"
+                                  slot-scope="data">
+                            <v-chip
+                                    :selected="data.selected"
+                                    :disabled="data.disabled"
+                                    :key="JSON.stringify(data.item)"
+                                    class="v-chip--select-multi"
+                                    @input="data.parent.selectItem(data.item)"
+                            > {{ data.item.name }}
+                            </v-chip>
+                        </template>
                     </v-combobox>
 
                 </v-card-text>
@@ -42,6 +59,15 @@ export default {
     }
   },
   methods: {
+    removeTag () {
+      // TODO ASYNC PRIMER EXECUTAR UN CONFIRM
+      console.log('TODO REMOVE TAG')
+      window.axios.delete('/api/v1/tasks/' + this.task.id + '/tag/' + this.tag).then(response => {
+        this.$snackbar.showMessage('Etiqueta eliminada correctament')
+      }).catch(error => {
+        this.$snackbar.showError(error)
+      })
+    },
     addTag () {
       console.log('TODO ADD TAG')
       let tag = {}

74ed95fa9ab1922d87ba25fa9fb81d3ac8828986
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2019-01-18 16:01:01 +0100

    Sync

--- app/Http/Controllers/Api/CompletedTasksController.php
+++ app/Http/Controllers/Api/CompletedTasksController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Api;
 
 
+use App\Log;
 use App\Task;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class CompletedTasksController
@@ -12,6 +14,21 @@ class CompletedTasksController
     {
         $task->completed=false;
         $task->save();
+
+        // EXEMPLE COM NO FER
+        Log::create([
+            'text' => "S'ha marcat com a pendent la tasca 'comprar pa'",
+            'time' => Carbon::now(),
+            'action_type'=> 'descompletar',
+            'module_type' => 'Tasques',
+            'icon' => 'lock_open',
+            'color' => 'primary',
+            'user_id' => $request->user()->id,
+            'loggable_id' => $task->id,
+            'loggable_type' => Task::class,
+            'old_value' => true,
+            'new_value' => false
+        ]);
     }
 
     public function store(Request $request, Task $task)
--- tests/Feature/Api/CompletedTaskControllerTest.php
+++ tests/Feature/Api/CompletedTaskControllerTest.php
@@ -2,8 +2,11 @@
 
 namespace Tests\Feature\Api;
 
+use App\Log;
+use App\Mail\TaskUncompleted;
 use App\Task;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Mail;
 use Tests\Feature\Traits\CanLogin;
 use Tests\TestCase;
 
@@ -42,16 +45,38 @@ class CompletedTaskControllerTest extends TestCase {
      */
     public function can_uncomplete_a_task()
     {
-        $this->login('api');
+        $user = $this->login('api');
         $task= Task::create([
             'name' => 'comprar pa',
             'completed' => true
         ]);
         //2
+        Mail::fake();
         $response = $this->json('DELETE','/api/v1/completed_task/' . $task->id);
         $response->assertSuccessful();
         $task = $task->fresh();
         $this->assertEquals((boolean) $task->completed, false);
+
+        // Test log is inserted
+        $log  = Log::find(1);
+        $this->assertEquals($log->text,"S'ha marcat com a pendent la tasca 'comprar pa'");
+        $this->assertEquals($log->action_type,'descompletar');
+        $this->assertEquals($log->module_type,'Tasques');
+        $this->assertEquals($log->user_id,$user->id);
+        $this->assertEquals($log->old_value,true);
+        $this->assertEquals($log->new_value,0);
+        $this->assertEquals($log->loggable_id,$task->id);
+        $this->assertEquals($log->loggable_type,Task::class);
+        $this->assertEquals($log->icon,'lock_open');
+        $this->assertEquals($log->color,'primary');
+
+        // Comprovar enviament Email
+        Mail::assertSent(TaskUncompleted::class, function ($mail) use ($task, $user) {
+//            return $mail->task->id === $task->id;
+            return $mail->task->is($task) &&
+                   $mail->hasTo($user->email) &&
+                   $mail->hasCc(config('tasks.manager_email'));
+        });
     }
 
     /**
